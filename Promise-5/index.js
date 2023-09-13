742a351bba3b8ded31187876913b95c99b890ffd
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-09-12 21:42:51 -0700

    Finished Promise 6

--- Promise-5/index.js
+++ Promise-5/index.js
@@ -1,6 +1,18 @@
 function whatToEat(timeOfDay) {
   // Create a new promise and return it here
-  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (timeOfDay === 'breakfast') {
+        resolve('Pancakes!')
+      } else if(timeOfDay === 'lunch') {
+        resolve('Pasta!')
+      } else if (timeOfDay === 'dinner') {
+        resolve('Steak!')
+      } else {
+        reject('Please enter breakfast, lunch or dinner and try again.')
+      }
+    }, 3333);
+  });
 }
 
 // whatToEat should return a promise. 
@@ -12,6 +24,16 @@ function whatToEat(timeOfDay) {
 // Handle the Promise returned from whatToEat here: 
 
 whatToEat('lunch')
+.then((message) => {
+  console.log(message);
+}).catch((err) => {
+  console.log(err);
+});
 
 // Handle this Promise: 
 whatToEat('elevens')
+.then((message) => {
+  console.log(message);
+}).catch((err) => {
+  console.log(err);
+});
